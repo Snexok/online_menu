8cf2597265ebc8d8b3ce517573809b000f64e26c
K.Darlin <konstantdarl@gmail.com>
2024-04-17 15:07:00 +0300

    Added classNames lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, { Suspense, useContext, useState } from 'react';
+import { Suspense } from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import { ThemeContext } from './theme/ThemeContext';
 import { useTheme } from './theme/useTheme';
+import classNames from 'classnames';
 
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme()
+    const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames("app", theme)}>
             <button onClick={toggleTheme}>T</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
