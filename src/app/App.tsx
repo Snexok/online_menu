965d7eb7e62af887336688c0172cd192aee5f5a7
K.Darlin <konstantdarl@gmail.com>
2024-04-21 21:16:04 +0300

    Added widget Loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import { MainPage } from 'pages/MainPage';
 import { useTheme } from 'shared/config/ThemeProvider';
 import { NavBar } from 'widgets/NavBar';
 import { ErrorPage } from 'pages/ErrorPage';
+import { Loader } from 'widgets/Loader';
 
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
         <div className={classNames("app", theme)}>
             <NavBar/>
             <div className="content">
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<Loader/>}>
                     <Routes>
                         <Route path={'/about'} element={<AboutPage />} />
                         <Route path={'/'} element={<MainPage />} />
